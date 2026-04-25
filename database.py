1d263258aeb4253060c414e84aa3af2caad6d1d2
Vitor Santos <mrvitorsantos@gmail.com>
2026-04-25 02:40:31 -0300

    Hardcoding Supabase credentials for deployment

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-url: str = os.getenv("SUPABASE_URL", "https://SUA_URL_DO_SUPABASE_AQUI.supabase.co")
-key: str = os.getenv("SUPABASE_KEY", "")
+url: str = os.getenv("SUPABASE_URL", "https://zwejekcpbphkkpdmpckx.supabase.co")
+key: str = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3ZWpla2NwYnBoa2twZG1wY2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwNjY0MjAsImV4cCI6MjA5MjY0MjQyMH0.HD9_EJ8GNM4wTTAylq1G_KjU5v-nl1AYsprPDT5-Ld4")
 
 supabase: Client = create_client(url, key)
